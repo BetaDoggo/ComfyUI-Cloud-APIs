f537e1761e40393cbe44d3192be956ddb6d82a7a
Alex Nunet <alexnunet@gmail.com>
2024-08-07 19:36:20 -0400

    Add img2img with Flux on fal (FalFluxI2IAPI)

--- nodes.py
+++ nodes.py
@@ -1,5 +1,6 @@
 import fal_client
 import replicate
+import base64
 import torch
 import requests
 import numpy as np
@@ -53,6 +54,77 @@ class FalAuraFlowAPI:
         output_image = torch.from_numpy(image)[None,]
         return (output_image,)
 
+class FalFluxI2IAPI:
+    @classmethod
+    def INPUT_TYPES(cls):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        api_keys = [f for f in os.listdir(os.path.join(current_dir, "keys")) if f.endswith('.txt')]
+        return {
+            "required": {
+                "image": ("IMAGE", {"forceInput": True,}),
+                "prompt": ("STRING", {"multiline": True}),
+                "strength": ("FLOAT", {"default": 0.90, "min": 0.01, "max": 1, "step": 0.01}),
+                "steps": ("INT", {"default": 25, "min": 1, "max": 50}),
+                "api_key": (api_keys,),
+                "seed": ("INT", {"default": 1337, "min": 1, "max": 16777215}),
+                "cfg": ("FLOAT", {"default": 3.5, "min": 1, "max": 20, "step": 0.5, "forceInput": False}),
+                "no_downscale": ("BOOLEAN", {"default": False,}),
+            },
+        }
+    
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "generate_image"
+    CATEGORY = "ComfyCloudAPIs"
+
+    def generate_image(self, image, prompt, strength, steps, api_key, seed, cfg, no_downscale):
+        #Convert from image tensor to array
+        image_np = 255. * image.cpu().numpy().squeeze()
+        image_np = np.clip(image_np, 0, 255).astype(np.uint8)
+        img = Image.fromarray(image_np).convert('L')
+        #downscale image to prevent excess cost
+        width, height = img.size #get size for checking
+        max_dimension = max(width, height)
+        scale_factor = 1024 / max_dimension
+        if scale_factor < 1 and not no_downscale:
+            new_width = int(width * scale_factor)
+            new_height = int(height * scale_factor)
+            img = img.resize((new_width, new_height), Image.LANCZOS)
+        width, height = img.size #get size for api
+        #upload image
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        file = buffered.getvalue()
+        image_url = fal_client.upload(file, "image/png")
+        #Set api key
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(os.path.join(current_dir, "keys"), api_key), 'r', encoding='utf-8') as file:
+            key = file.read()
+        os.environ["FAL_KEY"] = key
+        handler = fal_client.submit(
+        "fal-ai/flux/dev/image-to-image",
+        arguments={
+            "image_url": image_url,
+            "prompt": prompt,
+            "seed": seed,
+            "steps": steps,
+            "image_size": {
+                "width": width,
+                "height": height},
+            "strength": strength,
+            "guidance_scale": cfg,
+            "num_inference_steps": steps,
+            "num_images": 1, #Hardcoded to 1 for now
+        })
+        result = handler.get()
+        image_url = result['images'][0]['url']
+        #Download the image
+        response = requests.get(image_url)
+        image = Image.open(io.BytesIO(response.content))
+        #make image more comfy
+        image = np.array(image).astype(np.float32) / 255.0
+        output_image = torch.from_numpy(image)[None,]
+        return (output_image,)
+
 class FluxResolutionPresets:
     @classmethod
     def INPUT_TYPES(cls):
@@ -91,8 +163,8 @@ class FalFluxAPI:
             "required": {
                 "prompt": ("STRING", {"multiline": True}),
                 "endpoint": (["schnell (4+ steps)", "dev (25 steps)", "pro (25 steps)"],),
-                "width": ("INT", {"default": 1024, "min": 256, "max": 2048, "step": 64, "forceInput": False}),
-                "height": ("INT", {"default": 1024, "min": 256, "max": 2048, "step": 64, "forceInput": False}),
+                "width": ("INT", {"default": 1024, "min": 256, "max": 2048, "step": 16, "forceInput": False}),
+                "height": ("INT", {"default": 1024, "min": 256, "max": 2048, "step": 16, "forceInput": False}),
                 "steps": ("INT", {"default": 4, "min": 1, "max": 50}),
                 "api_key": (api_keys,),
                 "seed": ("INT", {"default": 1337, "min": 1, "max": 16777215}),
@@ -203,6 +275,7 @@ NODE_CLASS_MAPPINGS = {
     "ReplicateFluxAPI": ReplicateFluxAPI,
     "FluxResolutionPresets": FluxResolutionPresets,
     "FalAuraFlowAPI": FalAuraFlowAPI,
+    "FalFluxI2IAPI": FalFluxI2IAPI,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -210,4 +283,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ReplicateFluxAPI": "ReplicateFluxAPI",
     "FluxResolutionPresets": "FluxResolutionPresets",
     "FalAuraFlowAPI": "FalAuraFlowAPI",
+    "FalFluxI2IAPI": "FalFluxI2IAPI",
 }
